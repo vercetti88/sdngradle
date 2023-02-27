7e006c76acab6091a8052ade49b76e24388d82f2
Abdulaziz Baisarov <quntxm@gmail.com>
2023-02-28 00:34:32 +0600

    mycom

---
+++ src/main/java/com/sdnnn/project/entities/Person.java
@@ -0,0 +1,37 @@
+package com.sdnnn.project.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "iin")
+    private String iin;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "number")
+    private String phone;
+
+    @Column(name = "promocode")
+    private String promocode;
+
+
+    public Person(String iin, String name, String phone){
+        this.iin = iin;
+        this.name = name;
+        this.phone = phone;
+    }
+}
